ecc611c969bf335b4a49e0070c17bf3fda88d552
allimonae <esera.lee093@gmail.com>
2025-09-17 23:18:33 -0400

    installed daisyui, removed old styling

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,74 +1,74 @@
 import { useNavigate } from "react-router-dom";
 
 function LoginTextInput(props) {
-   return (
-      <div>
-         <label id={props.id}>
-            {props.title}:
-            <br />
-            <input type="text" />
-         </label>
-      </div>
-   );
+  return (
+    <div>
+        <label id={props.id}>
+          {props.title}:
+          <br />
+          <input type="text" />
+        </label>
+    </div>
+  );
 }
 
 function LoginPageButton(props) {
-   return (
-      <div>
-         <button onClick={props.func}>{props.title}</button>
-      </div>
-   );
+  return (
+    <div>
+      <button onClick={props.func}>{props.title}</button>
+    </div>
+  );
 }
 
 export default function LoginPage() {
-   let navigate = useNavigate();
+  let navigate = useNavigate();
 
-   {
-      /* Logic for when authentication is good. Empty for now */
-   }
-   const onSuccessfulLogin = () => {
-      {
-         /** Store credentials here */
-      }
+  {
+    /* Logic for when authentication is good. Empty for now */
+  }
+  const onSuccessfulLogin = () => {
+    {
+        /** Store credentials here */
+    }
 
-      navigate("/main");
-   };
+    navigate("/main");
+  };
 
-   {
-      /* Login button click logic. Empty for now */
-   }
-   const loginButtonClicked = () => {
-      {
-         /** Send info to server here */
-      }
+  {
+    /* Login button click logic. Empty for now */
+  }
+  const loginButtonClicked = () => {
+    {
+        /** Send info to server here */
+    }
 
-      if (true) {
-         onSuccessfulLogin();
-      }
-   };
+    if (true) {
+        onSuccessfulLogin();
+    }
+  };
 
-   return (
+  return (
+    <div>
       <div>
-         <div>
-            <h1>Login</h1>
-         </div>
-
-         <LoginTextInput
-            title="Username"
-            id="LoginPageUsernameField"
-         ></LoginTextInput>
-         
-         <LoginTextInput
-            title="Password"
-            id="LoginPagePasswordField"
-            type="password"
-         ></LoginTextInput>
-
-         <LoginPageButton
-            title="Login!"
-            func={loginButtonClicked}
-         ></LoginPageButton>
-         {/* TODO: Add way to reset password*/}
+        <h1>Login</h1>
       </div>
-   );
+
+      <LoginTextInput
+        title="Username"
+        id="LoginPageUsernameField"
+      ></LoginTextInput>
+      
+      <LoginTextInput
+        title="Password"
+        id="LoginPagePasswordField"
+        type="password"
+      ></LoginTextInput>
+
+      <LoginPageButton
+        title="Login!"
+        func={loginButtonClicked}
+      ></LoginPageButton>
+      {/* TODO: Add way to reset password*/}
+    </div>
+  );
 }
