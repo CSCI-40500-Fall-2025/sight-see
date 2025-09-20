c314101d4489e651557dec1abf2d40856303aad2
Kelvin Capriel Reyes <kelvincapriel@gmail.com>
2025-09-20 15:02:26 -0400

    Added temp way to navigate from MainPage to PostPage

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,3 +1,20 @@
+import { useNavigate } from "react-router-dom";
+
 export default function MainPage() {
-  return <h1>Main Page</h1>
+   const postId = "sample";
+   let navigate = useNavigate();
+
+   function buttonClick(postId) {
+      navigate(`/post/${postId}`);
+   }
+
+   return (
+      <div>
+         <h1>Main Page</h1>
+         {
+            // Temp button to go to post
+         }
+         <button onClick={() => buttonClick(postId)}>Go to post</button>
+      </div>
+   );
 }
