2457d3a0e64c6b4d87d8748a6a37bf4972600a85
Kelvin Capriel Reyes <kelvincapriel@gmail.com>
2025-09-20 14:35:53 -0400

    Changed sign up page to use states

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,17 +1,38 @@
 import { useNavigate } from "react-router-dom";
+import { use, useState } from "react";
 
+{
+   /** SignUp Text Input component
+    * Props:
+    *    id:         which of (username, password, email) input is
+    *    title:      The label for the text input
+    *    type:       type for input tag (mainly for password input box, to hid characters)
+    *    value:      which state variable the input box is binded to
+    *    func:       function to update respective state variable on change
+    */
+}
 function SignUpTextInput(props) {
    return (
       <div>
          <label id={props.id}>
             {props.title}:
             <br />
-            <input type="text" />
+            <input
+               type={props.type}
+               value={props.value}
+               onChange={(e) => props.func(e.target.value)}
+            />
          </label>
       </div>
    );
 }
-
+{
+   /** SignUp Button
+    * Props:
+    *    title:      text on button
+    *    func:       function that occurs on button click
+    */
+}
 function SignUpPageButton(props) {
    return (
       <div>
@@ -21,7 +42,10 @@ function SignUpPageButton(props) {
 }
 
 export default function SignUpPage() {
-   let navigate = useNavigate();
+   const navigate = useNavigate();
+   const [email, updateEmailState] = useState("");
+   const [username, updateUsernameState] = useState("");
+   const [password, updatePassWordState] = useState("");
 
    {
       /* Logic for when authentication is good. Empty for now */
@@ -56,15 +80,23 @@ export default function SignUpPage() {
          <SignUpTextInput
             title="Email"
             id="SignUpPageEmailField"
+            type="text"
+            value={email}
+            func={updateEmailState}
          ></SignUpTextInput>
          <SignUpTextInput
             title="Username"
             id="SignUpPageUsernameField"
+            type="text"
+            value={username}
+            func={updateUsernameState}
          ></SignUpTextInput>
-         {/* TODO: Make text for password input hidden somehow */}
          <SignUpTextInput
             title="Password"
             id="SignUpPagePasswordField"
+            type="password"
+            value={password}
+            func={updatePassWordState}
          ></SignUpTextInput>
 
          <SignUpPageButton
