12656901ad0023d8fbac654cb07ac0710c11c0ab
Austin Robertson <162934591+arobertson7@users.noreply.github.com>
2025-10-16 22:32:26 -0400

    implemented getAllPosts endpoint

--- sightsee-backend/src/main/java/io/github/CSCI_40500_Fall_2025/sightsee/sightsee_backend/post/PostController.java
+++ sightsee-backend/src/main/java/io/github/CSCI_40500_Fall_2025/sightsee/sightsee_backend/post/PostController.java
@@ -1,7 +1,20 @@
 package io.github.CSCI_40500_Fall_2025.sightsee.sightsee_backend.post;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 public class PostController {
+
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping("posts")
+    public List<Post> getAllPosts() {
+        return postService.getAllPosts();
+    }
 }
--- sightsee-backend/src/main/java/io/github/CSCI_40500_Fall_2025/sightsee/sightsee_backend/post/PostService.java
+++ sightsee-backend/src/main/java/io/github/CSCI_40500_Fall_2025/sightsee/sightsee_backend/post/PostService.java
@@ -2,6 +2,18 @@ package io.github.CSCI_40500_Fall_2025.sightsee.sightsee_backend.post;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PostService {
+
+    private final PostRepository postRepository;
+
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    public List<Post> getAllPosts() {
+        return postRepository.findAll();
+    }
 }
