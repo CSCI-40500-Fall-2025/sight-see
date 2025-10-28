6d63d6ba657924786dcc90e0f3ffd49a628aad46
Kelvin Capriel Reyes <kelvincapriel@gmail.com>
2025-10-28 18:17:13 -0400

    CreatePostPage: add location retrieval

--- frontend/src/pages/CreatePostPage.jsx
+++ frontend/src/pages/CreatePostPage.jsx
@@ -22,27 +22,25 @@ export default function CreatePostPage() {
       setCaption(truncated);
    };
 
-   const handleSubmit = () => {
-      const isValidCaption = validateCaptionInput(caption);
+   // Function to get the current location of user
+   const getLocation = async () => {
+      return new Promise((resolve, reject) => {
+         // https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/getCurrentPosition
+         if (navigator.geolocation) {
+            // Function that gets the user's current location
+            navigator.geolocation.getCurrentPosition(
+               // Location is good, return coordinates
+               (pos) => resolve(pos.coords),
 
-      if (!isValidCaption) {
-         // Error handling TODO
-         return;
-      }
-
-      console.log("GOOD");
-
-      // Get date and time
-      const time = Date.now();
-      console.log(time);
-
-      // Get location
-
-      // Get image?
-
-      // send to backend
-
-      //
+               // Error in getting location
+               (err) => reject(err)
+            );
+         }
+         // Geolocation not supported by user's device/browser
+         else {
+            reject("Location not supported");
+         }
+      });
    };
 
    /** Use regular expressions to validate caption input
@@ -51,7 +49,7 @@ export default function CreatePostPage() {
     *    Must only contain letters, numbers, and punctuation or emojis?
     *    Check for SQl injection
     */
-   const validateCaptionInput = (text) => {
+   const validateCaptionInput = () => {
       const characterRegex = /^[\p{L}\p{N}\p{P}\p{Zs}\p{Emoji}]{0,280}$/u;
 
       // Case Sensitive: will not match 'select'
@@ -76,7 +74,7 @@ export default function CreatePostPage() {
       const bannedCharacters = /['";\\\-*\/()=<>]/;
 
       // Check that has only allowed characters and is within allowed length
-      const containsAllowedCharacters = characterRegex.test(text);
+      const containsAllowedCharacters = characterRegex.test(caption);
 
       if (!containsAllowedCharacters) {
          // Error handle TODO
@@ -85,7 +83,7 @@ export default function CreatePostPage() {
       }
 
       // Check that there are no banned keywords
-      const containsBannedWords = bannedWordsRegex.test(text);
+      const containsBannedWords = bannedWordsRegex.test(caption);
 
       if (containsBannedWords) {
          // Error handle TODO
@@ -95,7 +93,7 @@ export default function CreatePostPage() {
       }
 
       // Check that there are no banned characters
-      const containsBannedCharacters = bannedCharacters.test(text);
+      const containsBannedCharacters = bannedCharacters.test(caption);
 
       if (containsBannedCharacters) {
          // Error handle TODO
@@ -106,6 +104,36 @@ export default function CreatePostPage() {
 
       // String is good
       return true;
+   };
+
+   const handleSubmit = async () => {
+      const isValidCaption = validateCaptionInput(caption);
+
+      if (!isValidCaption) {
+         // Error handling TODO
+         return;
+      }
+
+      console.log("GOOD");
+
+      // Get date and time
+      const time = Date.now();
+      console.log(time);
+
+      // Get image?
+
+      // Async logic
+      try {
+         // Get location
+         const { latitude, longitude } = await getLocation();
+
+         console.log("Lat: ", latitude);
+         console.log("Long:", longitude);
+
+         // Post to backend here
+      } catch (error) {
+         console.log(error);
+      }
    };
 
    return (
@@ -152,27 +180,31 @@ export default function CreatePostPage() {
 
 {
    /** Flow:
-    *       On create button click from main page
+    *       On create button click from main page: under profile dropdown
     *       Nav to new page
-    *       Here, you can upload an image (temp, will change to taking a picture soon!)
-    *       Once the picture is uploaded, display it and bring up a caption input box
+    *       Here, you can upload an image (temp, will change to taking a picture soon!)   : ImageUpload component
+    *       Once the picture is uploaded, display it and bring up a caption input box     : ImageDisplay and TextInput components
     *       Here the user inputs a short comment
-    *          Validate: maybe short captions only? SQL injection, newines, emojis
-    *       Once the comment is good:
+    *          Validate: maybe short captions only                                        : handlecaptionChange
+    *       Once the comment is good:                                                     : NOT present
     *             Present a button that will signal to user to create post
-    *       Once the button is pressed
-    *          Try to get the location of user first
+    *
+    *       Once the button is pressed                                                    : handleSubmit
+    *
+    *          Further validate comment input text                                        : validateCaptionInput
+    *          Get the image data                                                         : NOT present
+    *          Try to get the location of user first                                      : getLocation
     *             if fail, then stop and bring up error
-    *          If location get is good
     *          Package everything and post to backend
     *
-    *       Wait for response
-    *          If good, do something TODO
-    *          If bad, post error
+    *          Wait for response
+    *             If good, do something TODO
+    *                Suggestions: navigate back to main page with some sort of refresh happening to the map?
+    *             If bad, post error
     *
     *
     *    Potential Issues:
-    *       IPhone image format is NOT supported on all major browsers (only Safari)
+    *       IPhone image format is NOT supported on all major browsers (only Safari (and maybe IOS browsers using WebKit (so all of them)))
     *          So, any images in this format must be changed!
     *
     *          Ignore for now, since this will most likely depends on how taking a picture will be implemented
